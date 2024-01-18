b48a2d8af63261a1f03abd325c9fa4cf8034bf40
daati35 <125276422+dgegia16@users.noreply.github.com>
2024-01-18 16:48:56 +0400

    First Card appereance

--- asynchronous-javascript/asynchronous-project/script.js
+++ asynchronous-javascript/asynchronous-project/script.js
@@ -5,4 +5,30 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-const request = new XMLHttpRequest
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const data = JSON.parse(this.responseText)[1];
+    console.log(data);
+
+    const html = `
+        <article class="country">
+          <img class="country__img" src="${data.flags.png}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${data.population} people</p>
+            <p class="country__row"><span>🗣️</span>LANG: ${data.languages.kat}</p>
+            <p class="country__row"><span>💰</span>CUR: ${data.currencies.GEL.symbol}</p>
+          </div>
+        </article>
+       `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('Georgia')
