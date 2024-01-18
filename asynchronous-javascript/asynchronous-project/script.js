a3d572fd3e4d75851dd43c21303ffcf0e69adaf6
daati35 <125276422+dgegia16@users.noreply.github.com>
2024-01-18 18:18:58 +0400

    fixed more than one element in array

--- asynchronous-javascript/asynchronous-project/script.js
+++ asynchronous-javascript/asynchronous-project/script.js
@@ -11,24 +11,58 @@ const getCountryData = function (country) {
   request.send();
 
   request.addEventListener('load', function () {
-    const data = JSON.parse(this.responseText)[1];
-    console.log(data);
+    const data = JSON.parse(this.responseText);
 
-    const html = `
+    if (data.length > 1) {
+      data.forEach(countryData => {
+        console.log(countryData);
+
+        const html = `
+          <article class="country">
+            <img class="country__img" src="${countryData.flags.png}" />
+            <div class="country__data">
+              <h3 class="country__name">${countryData.name.common}</h3>
+              <h4 class="country__region">${countryData.region}</h4>
+              <p class="country__row"><span>👫</span>${
+                countryData.population
+              } people</p>
+              <p class="country__row"><span>🗣️</span>LANG: ${
+                Object.values(countryData.languages)[0]
+              }</p>
+            </div>
+          </article>
+        `;
+        countriesContainer.insertAdjacentHTML('beforeend', html);
+      });
+
+      countriesContainer.style.opacity = 1;
+    } else if (data.length === 1) {
+      const countryData = data[0];
+      console.log(countryData);
+
+      const html = `
         <article class="country">
-          <img class="country__img" src="${data.flags.png}" />
+          <img class="country__img" src="${countryData.flags.png}" />
           <div class="country__data">
-            <h3 class="country__name">${data.name.common}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${data.population} people</p>
-            <p class="country__row"><span>🗣️</span>LANG: ${data.languages.kat}</p>
-            <p class="country__row"><span>💰</span>CUR: ${data.currencies.GEL.symbol}</p>
+            <h3 class="country__name">${countryData.name.common}</h3>
+            <h4 class="country__region">${countryData.region}</h4>
+            <p class="country__row"><span>👫</span>${
+              countryData.population
+            } people</p>
+            <p class="country__row"><span>🗣️</span>LANG: ${
+              Object.values(countryData.languages)[0]
+            }</p>
           </div>
         </article>
-       `;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+      `;
+      countriesContainer.insertAdjacentHTML('beforeend', html);
+
+      countriesContainer.style.opacity = 1;
+    }
   });
 };
 
 getCountryData('Georgia')
+
+
+
