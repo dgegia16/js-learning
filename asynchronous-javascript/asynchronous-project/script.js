c458806892abc64cd03b749560ce8717a929b9f3
daati35 <125276422+dgegia16@users.noreply.github.com>
2024-01-19 15:24:20 +0400

    neighbors dont work

--- asynchronous-javascript/asynchronous-project/script.js
+++ asynchronous-javascript/asynchronous-project/script.js
@@ -5,42 +5,96 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-  request.send();
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
 
-  request.addEventListener('load', function () {
-    const data = JSON.parse(this.responseText);
+//   request.addEventListener('load', function () {
+//     const data = JSON.parse(this.responseText);
 
-    if (data.length > 1) {
-      data.forEach(countryData => {
-        console.log(countryData);
+//     if (data.length > 1) {
+//       data.forEach(countryData => {
+//         console.log(countryData);
 
-        const html = `
+//         const html = `
+//           <article class="country">
+//             <img class="country__img" src="${countryData.flags.png}" />
+//             <div class="country__data">
+//               <h3 class="country__name">${countryData.name.common}</h3>
+//               <h4 class="country__region">${countryData.region}</h4>
+//               <p class="country__row"><span>👫</span>${
+//                 Math.floor((countryData.population / 1000000) * 100) / 100 + 'M'
+//               } people</p>
+//               <p class="country__row"><span>🗣️</span>LANG: ${
+//                 Object.values(countryData.languages)[0]
+//               }</p>
+//             <p class="country__row"><span>🏰</span>${countryData.capital}</p>
+//             </div>
+//           </article>
+//         `;
+//         countriesContainer.insertAdjacentHTML('beforeend', html);
+//       });
+
+//       countriesContainer.style.opacity = 1;
+//     } else if (data.length === 1) {
+//       const countryData = data[0];
+//       console.log(countryData);
+
+//       const html = `
+//         <article class="country">
+//           <img class="country__img" src="${countryData.flags.png}" />
+//           <div class="country__data">
+//             <h3 class="country__name">${countryData.name.common}</h3>
+//             <h4 class="country__region">${countryData.region}</h4>
+//             <p class="country__row"><span>👫</span>${
+//               countryData.population
+//             } people</p>
+//             <p class="country__row"><span>🗣️</span>LANG: ${
+//               Object.values(countryData.languages)[0]
+//             }</p>
+//             <p class="country__row"><span>🏰</span>${countryData.capital}</p>
+//             <p class="country__row"><span>💰</span>CUR: </p>
+//           </div>
+//         </article>
+//       `;
+//       countriesContainer.insertAdjacentHTML('beforeend', html);
+
+//       countriesContainer.style.opacity = 1;
+//     }
+//   });
+// };
+
+const renderCountry = function (data) {
+  if (data.length > 1) {
+    data.forEach(countryData => {
+      console.log(countryData);
+
+      const html = `
           <article class="country">
             <img class="country__img" src="${countryData.flags.png}" />
             <div class="country__data">
               <h3 class="country__name">${countryData.name.common}</h3>
               <h4 class="country__region">${countryData.region}</h4>
               <p class="country__row"><span>👫</span>${
-                countryData.population
+                Math.floor((countryData.population / 1000000) * 100) / 100 + 'M'
               } people</p>
               <p class="country__row"><span>🗣️</span>LANG: ${
                 Object.values(countryData.languages)[0]
               }</p>
+            <p class="country__row"><span>🏰</span>${countryData.capital}</p>
             </div>
           </article>
         `;
-        countriesContainer.insertAdjacentHTML('beforeend', html);
-      });
+      countriesContainer.insertAdjacentHTML('beforeend', html);
+    });
 
-      countriesContainer.style.opacity = 1;
-    } else if (data.length === 1) {
-      const countryData = data[0];
-      console.log(countryData);
+    countriesContainer.style.opacity = 1;
+  } else if (data.length === 1) {
+    const countryData = data[0];
+    console.log(countryData);
 
-      const html = `
+    const html = `
         <article class="country">
           <img class="country__img" src="${countryData.flags.png}" />
           <div class="country__data">
@@ -52,17 +106,40 @@ const getCountryData = function (country) {
             <p class="country__row"><span>🗣️</span>LANG: ${
               Object.values(countryData.languages)[0]
             }</p>
+            <p class="country__row"><span>🏰</span>${countryData.capital}</p>
+            <p class="country__row"><span>💰</span>CUR: </p>
           </div>
         </article>
       `;
-      countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.insertAdjacentHTML('beforeend', html);
 
-      countriesContainer.style.opacity = 1;
-    }
+    countriesContainer.style.opacity = 1;
+  }
+};
+
+const getCountryAndNeighbour = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const data = JSON.parse(this.responseText);
+
+    // Render country
+    renderCountry(data);
+
+    // Render its neighbor
+    const neighbour = data.borders?.[0];
+    console.log(data.borders);
+    if (!neighbour) return;
+    const request2 = new XMLHttpRequest();
+    request.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request.send();
+
+    request2.addEventListener('load', function(){
+      console.log(this.responseText);
+    })
   });
 };
 
-getCountryData('Georgia')
-
-
-
+getCountryAndNeighbour('namibia');
